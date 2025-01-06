e46cfc79769388dcf5392dac4115d7da2411bd54
rudraparmar76 <rudraparmar1309@gmail.com>
2025-01-06 20:41:53 +0530

    modify app.py

--- app.py
+++ app.py
@@ -80,5 +80,3 @@ def generate_prescription():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from app import app  # Replace 'app' with the name of your main Flask file/module
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
